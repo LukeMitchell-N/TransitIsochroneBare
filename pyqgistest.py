91521a79af95530fe837215908873a79f9a1c595
LukeMitchell-N <lukemitchellnelson@gmail.com>
2025-07-08 01:38:50 +0000

    Fix qgis project file type

--- pyqgistest.py
+++ pyqgistest.py
@@ -7,16 +7,16 @@ from qgis.analysis import QgsNativeAlgorithms
 
 # Supply path to qgis install location
 #path = r"C:\Program Files\QGIS 3.34.12\apps\qgis-ltr"      # Windows
-path = r"~/usr"                                              # Ubuntu
+path = r"/usr"                                              # Ubuntu
 QgsApplication.setPrefixPath(path, True)
 
 # Create a reference to the QgsApplication.  Setting the
 # second argument to False disables the GUI.
 os.environ["QT_QPA_PLATFORM"] = "offscreen"
-qgs = QgsApplication([], False)
+qgis = QgsApplication([], False)
 
 # Initialize a QGIS instance
-qgs.initQgis()
+qgis.initQgis()
 
 #sys.path.append(r"C:\Program Files\QGIS 3.34.12\apps\qgis-ltr\python\plugins")     # Windows
 sys.path.append('/usr/share/qgis/python/plugins')                                   # Ubuntu
@@ -26,9 +26,8 @@ from processing.core.Processing import Processing
 Processing.initialize()
 
 project = QgsProject.instance()
-project.read('./TransitConnectivity.qgz')
-print("test")
-print(f"layer count: {project.count()}, base name: {project.baseName()}")
+project.read('./TransitConnectivity.qgs')
+#print(f"layer count: {project.count()}, base name: {project.baseName()}")
 #for name in project.mapLayers().values():
 #    print(layer.name())
 
@@ -51,4 +50,4 @@ result = processing.run("alg_provider:transitservicearea", params)
 #
 # # Finally, exitQgis() is called to remove the
 # # provider and layer registries from memory
-qgs.exitQgis()
+qgis.exitQgis()

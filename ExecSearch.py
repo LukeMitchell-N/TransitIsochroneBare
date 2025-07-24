c3d2692a71aa3ee6a457982de7a0552176c7cca0
lukemitchell-n <lukemitchellnelson@gmail.com>
2025-07-24 11:12:07 -0700

    Added feedback and refactored

--- ExecSearch.py
+++ ExecSearch.py
@@ -4,52 +4,68 @@ from qgis.core import *
 from qgis import processing
 from qgis.analysis import QgsNativeAlgorithms
 from datetime import datetime
-
-
-# Supply path to qgis install location
-#path = r"C:\Program Files\QGIS 3.34.12\apps\qgis-ltr"      # Windows
-path = r"/usr"                                              # Ubuntu
-QgsApplication.setPrefixPath(path, True)
-
-# Create a reference to the QgsApplication.  Setting the
-# second argument to False disables the GUI.
-os.environ["QT_QPA_PLATFORM"] = "offscreen"
-qgis = QgsApplication([], False)
-
-# Initialize a QGIS instance
-qgis.initQgis()
-
 #sys.path.append(r"C:\Program Files\QGIS 3.34.12\apps\qgis-ltr\python\plugins")     # Windows
 sys.path.append('/usr/share/qgis/python/plugins')                                   # Ubuntu
-
 import processing
 from processing.core.Processing import Processing
-Processing.initialize()
 
+
+class ProcessFeeback(QgsProcessingFeedback):
+    def __init__(self):
+        super().__init__()
+        self.messages = []
+        self.progress = 0
+
+    def setProgress(self, progress):
+        self.progress = progress
+        self.messages.append(f'Progress: {progress}%')
+
+    def reportError(self, msg, fatalError=False):
+        self.messages.append(f'Error: {msg}')
+
+
+                                                            # Supply path to qgis install location
+#qgis_path = r"C:\Program Files\QGIS 3.34.12\apps\qgis-ltr"      # Windows path
+qgis_path = '/usr'                                               # Ubuntu path
+QgsApplication.setPrefixPath(qgis_path, True)
+
+os.environ["QT_QPA_PLATFORM"] = "offscreen"                 # Flag QT to be offscreen/headless
+qgis = QgsApplication([], False)                            # Ref to QGIS app, set to no GUI
+
+project_path = '/portfolio_app/processing/TransitIsochroneTool/TransitConnectivity.qgs'
 project = QgsProject.instance()
-project.read('./TransitConnectivity.qgs')
-#print(f"layer count: {project.count()}, base name: {project.baseName()}")
-#for name in project.mapLayers().values():
-#    print(layer.name())
 
-from AlgorithmProvider import AlgorithmProvider
-Processing.initialize()
-provider = AlgorithmProvider()
-QgsApplication.processingRegistry().addProvider(provider)
 
-current_time = datetime.now().strftime("%Y-%m-%d'%H'%M'%S")
 
-params = {
-        'STARTLOCATION'     : '7642700.835310,682883.097856 [EPSG:2913]',
-        'SEARCHTIMELIMIT'   : 5,
-        'TIMESTAMP'         : current_time,
-        'OUTPUT'            : 'TEMPORARY_OUTPUT'
-        }
+def run_isochrone(start_loc, time_limit):
 
-feedback = QgsProcessingFeedback()
-result = processing.run("alg_provider:transitservicearea", params)
-#
-#
-# # Finally, exitQgis() is called to remove the
-# # provider and layer registries from memory
-qgis.exitQgis()
+    # Initialize a QGIS instance
+    qgis.initQgis()
+    
+    project.read(project_path)    
+
+    from AlgorithmProvider import AlgorithmProvider             # Load algorithm provider only after reading in project
+    Processing.initialize()
+    provider = AlgorithmProvider()
+    QgsApplication.processingRegistry().addProvider(provider)
+
+    current_time = datetime.now().strftime("%Y-%m-%d'%H'%M'%S")
+
+    params = {
+            'STARTLOCATION'     : start_loc,
+            'SEARCHTIMELIMIT'   : time_limit,
+            'TIMESTAMP'         : current_time,
+            'OUTPUT'            : 'TEMPORARY_OUTPUT'
+            }
+
+    context = QgsProcessingContext()
+    context.setProject(project)
+
+    feedback = ProcessFeeback()
+
+    alg = QgsApplication.processingRegistry().algorithmById("alg_provider:transitservicearea")
+    alg.run(params, context, feedback)                          
+
+    qgis.exitQgis()
+
+run_isochrone('7642700.835310,682883.097856 [EPSG:2913]',5)
